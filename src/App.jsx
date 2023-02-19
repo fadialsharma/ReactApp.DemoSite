42f6d4abc8b429b30ea77242458725cc8d37278d
Fadi <35904824+fadi-e@users.noreply.github.com>
2023-02-19 14:12:39 +1000

    Task4: Product filter - Feature: Product filter feature to filter through products type

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Form, Dropdown } from 'react-bootstrap';
 import products from "./products.json";
 
 function App() {
+  const [filterType, setFilterType] = useState('');
+  const handleFilterChange = (event) => {
+    setFilterType(event.target.value);
+    // console.log(setFilterType);
+  }
+
   return (
     <Container>
-      <h1> Fadi's react app</h1>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group controlId="formTypeFilter">
+              <Form.Label>Filter by Type:</Form.Label>
+              <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  {filterType ? filterType : 'Select Type'}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={handleFilterChange} value="">All</Dropdown.Item>
+                  <Dropdown.Item onClick={handleFilterChange} value="Beer">Beer</Dropdown.Item>
+                  <Dropdown.Item onClick={handleFilterChange} value="Wine">Wine</Dropdown.Item>
+                  <Dropdown.Item onClick={handleFilterChange} value="Spirits">Spirits</Dropdown.Item>
+                  <Dropdown.Item onClick={handleFilterChange} value="Cider">Cider</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 }
