baf2e36b22b5924f5116b8ea6f4479c07e6be2c0
Fadi <35904824+fadi-e@users.noreply.github.com>
2023-02-20 01:49:04 +1000

    Task6: Display products - Feature: Responsive display for products - Fix: Assets size fixes - Add: CSS prefixes

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,28 +1,37 @@
 import { Row, Col, Card } from "react-bootstrap";
 import products from "../products.json";
-import classes from "./ProductsList.module.css"
+import classes from "./ProductsList.module.css";
 
 // console.log(products);
 
 function ProductsList() {
   const returnedProducts = products.filter((product) => {
-    return (
-      product
-    )
-  })
+    return product;
+  });
 
   return (
     <Row className={classes.CustomRow}>
       {returnedProducts.map((product) => (
         <Col sm={6} md={4} className={classes.CustomCol} key={product.index}>
           <Card.Text className={classes.CustomItemOnSaleContainer}>
-              <p className={classes.ItemOnSale}>{product.isSale ? "On Sale!" : null}</p>
-            </Card.Text>
+            <span
+              className={classes.ItemOnSale}
+              style={{ display: product.isSale ? "block" : "none" }}
+            >
+              On Sale!
+            </span>
+          </Card.Text>
           <Card className={classes.CustomCard}>
-            <Card.Img variant="top" className={classes.ProductImage} src={product.productImage} />
+            <Card.Img
+              variant="top"
+              className={classes.ProductImage}
+              src={product.productImage}
+            />
             <Card.Body className={classes.CustomCardBody}>
               <Card.Text className={classes.CustomCardTextContainer}>
-                <p className={classes.CustomProductName}>{product.productName}</p> 
+                <p className={classes.CustomProductName}>
+                  {product.productName}
+                </p>
                 <p className={classes.CustomProductPrice}>{product.price}</p>
               </Card.Text>
             </Card.Body>
@@ -30,7 +39,7 @@ function ProductsList() {
         </Col>
       ))}
     </Row>
-  )
+  );
 }
 
 export default ProductsList;
